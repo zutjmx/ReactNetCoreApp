815dd52af34f4f5b2df1598e9c8fe430f22671a8
Jesus Zuñiga <jesuszt@hotmail.com>
2023-05-25 15:00:16 -0600

    Se agrega paquete Nuget correcto para Faker y se agrega un controlador de prueba

---
+++ webapi/Comun/DatosFake.cs
@@ -0,0 +1,35 @@
+﻿namespace webapi.Comun
+{
+    public class DatosFake
+    {
+        public DatosFake()
+        {
+                
+        }
+
+        public string GeneraCadenaLorem(int cantidad)
+        {
+            string cadenaLorem;
+
+            if(cantidad <= 0)
+            {
+                cadenaLorem = Faker.Lorem.Paragraph();
+            } else
+            {
+                cadenaLorem = Faker.Lorem.Paragraph(cantidad);
+            }
+
+            return cadenaLorem;
+        }
+
+        public string[] GeneraArrCadenaLorem()
+        {
+            string[] cadenaLorem = new string[10];
+            for (int i = 0; i < cadenaLorem.Length; i++)
+            {
+                cadenaLorem[i] = Faker.Lorem.Paragraph();
+            }
+            return cadenaLorem;
+        }
+    }
+}
---
+++ webapi/Controllers/FakerController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using webapi.Comun;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace webapi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FakerController : ControllerBase
+    {
+
+        private readonly DatosFake datosFake = new DatosFake();
+
+        // GET: api/<FakerController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return datosFake.GeneraArrCadenaLorem();
+        }
+
+        // GET api/<FakerController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return datosFake.GeneraCadenaLorem(id);
+        }
+
+        // POST api/<FakerController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<FakerController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<FakerController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
